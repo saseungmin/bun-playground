f138fc1ac29517d5eb4f514f885b519deec29704
saseungmin <dbd02169@naver.com>
2023-12-30 02:38:32 +0900

    chore: eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,55 @@
 module.exports = {
-  "extends": "next/core-web-vitals"
-}
+  root: true,
+  ignorePatterns: [
+    '/.next',
+  ],
+  extends: [
+    '@nf-team/eslint-config',
+    'plugin:@next/next/recommended',
+  ],
+  plugins: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.js', '.tsx', '.json'],
+      },
+    },
+  },
+  rules: {
+    'react/require-default-props': 'off',
+    'import/order': 'off',
+    '@next/next/no-html-link-for-pages': ['error', 'app/'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        ['^\\u0000'],
+        ['^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'],
+        ['^react'],
+        ['^next'],
+        ['^@?\\w'],
+        ['^(@|lib|components|utils|hooks|stores)(/.*|$)'],
+        ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+        ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ['^.+\\.svg$'],
+        ['^.+\\.s?css$'],
+      ],
+    }],
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+  swcMinify: true,
+  experimental: {
+    typedRoutes: true,
+    esmExternals: 'loose',
+  },
+  compiler: {
+    reactRemoveProperties: isProd && {
+      properties: ['^data-test'],
+    },
+    removeConsole: isProd && {
+      exclude: ['error'],
+    },
+  },
+};
+
+module.exports = nextConfig;
